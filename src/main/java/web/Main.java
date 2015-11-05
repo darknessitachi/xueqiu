ce14b9afae4aa50a67527bd6904430033a23dae3
yangruie63 <yangrui891210@gmail.com>
2015-11-05 11:18:07 +0800

    commit

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -3,7 +3,7 @@ package web;
 import java.io.IOException;
 import java.util.Date;
 
-public class StockMain {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		long start = new Date().getTime();
