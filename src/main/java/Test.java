3d9cf3c410357ae66a1609fdef550f92262e0014
yangruie63 <yangrui891210@gmail.com>
2015-12-16 16:52:19 +0800

    commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("A1code/ddd.ebk" .substring(0,2));
+		System.out.println("A1code/ddd.ebk" .substring(1));
 	}
 	
 
