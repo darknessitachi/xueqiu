90cf2bd607a464fecbfe89d1147b0abdb858e96f
yangruie63 <yangrui891210@gmail.com>
2016-01-25 08:54:23 +0800

    commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,13 +3,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-
-
-
-
-
-
-
 public class Test {
 
 	public static void main(String[] args) throws IOException {
