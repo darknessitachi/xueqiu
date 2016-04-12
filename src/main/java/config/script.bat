91a6af26ccc789293655e6f888dd3a990114388b
yangruie63 <yangrui891210@gmail.com>
2016-04-12 08:31:23 +0800

    commit

--- src/main/java/config/script.bat
+++ src/main/java/config/script.bat
@@ -1,12 +1,5 @@
 @echo off 
-:: 删除class目录中的code目录文件
-del /Q D:\Documents\GitHub\xueqiu\target\classes\code\*
-:: 拷贝源码中的配置文件到class目录
-copy /y D:\Documents\GitHub\xueqiu\src\main\java\code D:\Documents\GitHub\xueqiu\target\classes\code
-copy /y D:\Documents\GitHub\xueqiu\src\main\java\config D:\Documents\GitHub\xueqiu\target\classes\config
-:: 删除class目录中不必要的文件
-del D:\Documents\GitHub\xueqiu\target\classes\config\Constants.java
-java -classpath "D:\Documents\GitHub\xueqiu\target\classes;C:\Users\Administrator\Documents\Downloads\json-lib\*"  gui.MainGui
+java -classpath "C:\Users\yangrui\Documents\GitHub\xueqiu\target\classes;C:\Users\yangrui\Downloads\json-lib-2.4\*;D:\workspaces\myeclipse\dy\tools\target\classes"  gui.MainGui
 exit
 
 
