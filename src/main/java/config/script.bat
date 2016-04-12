5850632bd87a211d4b2b9c3ca54390af8161efe0
yangruie63 <yangrui891210@gmail.com>
2016-04-12 15:21:00 +0800

    commit

--- src/main/java/config/script.bat
+++ src/main/java/config/script.bat
@@ -1,5 +1,5 @@
 @echo off 
-java -classpath "C:\Users\yangrui\Documents\GitHub\xueqiu\target\classes;C:\Users\yangrui\Downloads\json-lib-2.4\*;D:\workspaces\myeclipse\dy\tools\target\classes"  gui.MainGui
+java -classpath "C:\Users\yangrui\Documents\GitHub\xueqiu\target\classes;C:\Users\yangrui\Documents\GitHub\tools\target\classes;D:\worksoftware\xueqiu-lib\*"  gui.MainGui
 exit
 
 
