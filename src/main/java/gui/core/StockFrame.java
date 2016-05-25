144b6339ff757f16958a82e21d87dbbe396efa61
yangruie63 <yangrui891210@gmail.com>
2016-05-25 22:13:23 +0800

    增加wc文件的自动导入

--- src/main/java/gui/core/StockFrame.java
+++ src/main/java/gui/core/StockFrame.java
@@ -425,8 +425,10 @@ public class StockFrame extends JFrame implements ActionListener {
 					String targetName = null;
 					if("ZXG.BLK".equals(srcName.toUpperCase())){
 						targetName = "A1自选股.EBK";
+					}else if(srcName.toUpperCase().contains("A2GCG")){
+						targetName = "A2A2 观察股(25内).EBK";
 					}else{
-						targetName = "A9A2 观察股(25内).EBK";
+						targetName = "A9"+srcName.split("\\.")[0]+".EBK";
 					}
 					//拷贝到EBK目录
 					FileUtil.copy(Constants.out_custom_path+"/"+targetName, new File(path+"/"+srcName));
