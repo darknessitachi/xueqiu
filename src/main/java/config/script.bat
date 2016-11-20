b9087b074b7d9dc0152f827584177429ba73f196
yangruie63 <yangrui891210@gmail.com>
2016-11-20 22:01:50 +0800

    1

--- src/main/java/config/script.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-java -classpath "C:\Users\yangrui\Documents\GitHub\xueqiu\target\classes;C:\Users\yangrui\Documents\GitHub\tools\target\classes;D:\worksoftware\xueqiu-lib\*"  gui.MainGui
-exit
-
-
