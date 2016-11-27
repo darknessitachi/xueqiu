b2daa903a386d1ab611553ac9159cfd80001d55d
yangruie63 <yangrui891210@gmail.com>
2016-11-27 23:34:29 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,23 @@
 import java.io.IOException;
-
-import util.FileUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Test {
 		public static void main(String[] args) throws IOException {
-			FileUtil.copyDirectiory("d:/xueqiu", "c:/xueqiu");
+			
+			//System.out.println(FileUtil.read("d:/test.txt"));
+			
+			List<String> list = new ArrayList<String>();
+			
+			list.add("123");
+			//list.add("yangr");
+			
+			StringBuilder sb = new StringBuilder();
+			for(String s:list){
+				sb.append(",").append(s);
+			}
+			String resutl = "'"+sb.toString().substring(1).replace(",", "','")+"'";
+			System.out.println(resutl);
 		}
 }
