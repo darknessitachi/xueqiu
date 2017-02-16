197817ad7a191b7e272c63d5e9af899d31349c69
yangruie63 <yangrui891210@gmail.com>
2017-02-16 09:54:11 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 public class Test {
 		public static void main(String[] args) throws IOException {
 			
-			System.out.println("兔 宝 宝".replaceAll(" ", ""));
+			System.out.println("img/2017-02".substring(4));
 		}
 }
