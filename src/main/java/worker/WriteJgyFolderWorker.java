c2c580382e4584d8693a8b17d3b1f248650c4781
yangruie63 <yangrui891210@gmail.com>
2017-03-18 00:41:51 +0800

    1

--- src/main/java/worker/WriteJgyFolderWorker.java
+++ src/main/java/worker/WriteJgyFolderWorker.java
@@ -30,70 +30,92 @@ public class WriteJgyFolderWorker  {
 		StringBuilder msg = new StringBuilder();
 		for(String day:days){
 			String folder = Constants.jgy_path+"/"+day;
-			//如果文件夹不存在，则创建后写入
+			//如果文件夹不存在，则创建
 			if(!FileUtil.exists(folder)){
-				//创建文件夹
 				FileUtil.createFolder(folder);
+			}
 				
-				String preDay = null;
+			String preDay = null;
+			
+			String sql = " select r.*,s.code from record r left join stock s on stockName=name where type in ('1','2','3') and day='"+day+"' order by type asc,xh asc,rate desc,createDate desc ";
+			List<Map<String,Object>> list = MiniDbUtil.query(sql);
+			
+			//遍历一天中的数据
+			for(Map<String,Object> map:list){
+				String code = (String) map.get("code");
+				preDay = (String) map.get("preDay"); 
 				
-				String sql = " select r.*,s.code from record r left join stock s on stockName=name where type in ('1','2','3') and day='"+day+"' order by type asc,xh asc,rate desc,createDate desc ";
-				List<Map<String,Object>> list = MiniDbUtil.query(sql);
-				
-				for(Map<String,Object> map:list){
-					String code = (String) map.get("code");
-					preDay = (String) map.get("preDay"); 
-					
-					if(StringUtil.isEmpty(code)){
-						msg.append("【"+map.get("stockName")+"】未找到code").append("\n");
-						break;
-					}
-					
-					//导出反弹前的图片
-					String srcFileName = preDay+"_"+code.substring(1)+".png";
-					String targetFileName = day+"_"+code.substring(1)+"_0.png";
-					if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
-						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
-					}else{
-						msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
-					}
-					
-					//导出反弹后的图片
-					srcFileName = day+"_"+code.substring(1)+".png";
-					targetFileName = day+"_"+code.substring(1)+"_1.png";
-					if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
-						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
-					}else{
-						msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
-					}
-					
-					//导出分时图片
-					srcFileName = day+"_"+code.substring(1)+"_T.png";
-					targetFileName = day+"_"+code.substring(1)+"_2.png";
-					if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
-						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
-					}else{
-						msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
-					}
+				if(StringUtil.isEmpty(code)){
+					msg.append("【"+map.get("stockName")+"】未找到code").append("\n");
+					break;
 				}
 				
-				if(!StringUtil.isEmpty(preDay)){
-					//最后写入大盘的照片
-					if(FileUtil.exists(Constants.out_img_path+"/"+preDay+"_SH.png")  ){
+				//导出反弹前的图片
+				String srcFileName = preDay+"_"+code.substring(1)+".png";
+				String targetFileName = day+"_"+code.substring(1)+"_0.png";
+				if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+targetFileName)){
+						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
+					}
+				}else{
+					msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
+				}
+				
+				//导出反弹后的图片
+				srcFileName = day+"_"+code.substring(1)+".png";
+				targetFileName = day+"_"+code.substring(1)+"_1.png";
+				if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+targetFileName)){
+						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
+					}
+				}else{
+					msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
+				}
+				
+				//导出分时图片
+				srcFileName = day+"_"+code.substring(1)+"_T.png";
+				targetFileName = day+"_"+code.substring(1)+"_2.png";
+				if(FileUtil.exists(Constants.out_img_path+"/"+srcFileName)){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+targetFileName)){
+						FileUtil.copy(folder+"/"+targetFileName, new File(Constants.out_img_path+"/"+srcFileName));
+					}
+				}else{
+					msg.append("资源【"+Constants.out_img_path+"/"+srcFileName+"】未找到").append("\n");
+				}
+			}
+			
+			if(!StringUtil.isEmpty(preDay)){
+				//最后写入大盘的照片
+				if(FileUtil.exists(Constants.out_img_path+"/"+preDay+"_SH.png")  ){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+day+"_000SH_0.png")){
 						FileUtil.copy(folder+"/"+day+"_000SH_0.png", new File(Constants.out_img_path+"/"+preDay+"_SH.png"));
 					}
-					if(FileUtil.exists(Constants.out_img_path+"/"+preDay+"_CYB.png")  ){
+				}
+				if(FileUtil.exists(Constants.out_img_path+"/"+preDay+"_CYB.png")  ){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+day+"_000CYB_0.png")){
 						FileUtil.copy(folder+"/"+day+"_000CYB_0.png", new File(Constants.out_img_path+"/"+preDay+"_CYB.png"));
 					}
-					if(FileUtil.exists(Constants.out_img_path+"/"+day+"_SH.png")  ){
+				}
+				if(FileUtil.exists(Constants.out_img_path+"/"+day+"_SH.png")  ){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+day+"_000SH_1.png")){
 						FileUtil.copy(folder+"/"+day+"_000SH_1.png", new File(Constants.out_img_path+"/"+day+"_SH.png"));
 					}
-					if(FileUtil.exists(Constants.out_img_path+"/"+day+"_CYB.png")  ){
+				}
+				if(FileUtil.exists(Constants.out_img_path+"/"+day+"_CYB.png")  ){
+					//如果目标不存在，则写入
+					if(!FileUtil.exists(folder+"/"+day+"_000CYB_1.png")){
 						FileUtil.copy(folder+"/"+day+"_000CYB_1.png", new File(Constants.out_img_path+"/"+day+"_CYB.png"));
 					}
 				}
-				
 			}
+				
+			
 		}
 		
 		System.err.println(msg.toString());
