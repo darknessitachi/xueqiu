9f7252c063165b58f2323895cb41e59d3377e352
yangruie63 <yangrui891210@gmail.com>
2017-03-21 14:41:43 +0800

    1

--- src/main/java/worker/WriteJgyFolderWorker.java
+++ src/main/java/worker/WriteJgyFolderWorker.java
@@ -298,13 +298,13 @@ public class WriteJgyFolderWorker  {
 				String beforePath = Constants.jgy_path+"/"+folderName+"/before";
 				String mistakePath = Constants.jgy_path+"/"+folderName+"/mistake";
 				
-				int recordCount = 0;
-				int mistakeCount = 0;
+				String recordCount = "0";
+				String mistakeCount = "-";
 				if(FileUtil.exists(beforePath)){
-					recordCount = new File(beforePath).list().length;
+					recordCount = (new File(beforePath).list().length)+"";
 				}
 				if(FileUtil.exists(mistakePath)){
-					mistakeCount = (new File(mistakePath).list().length)/2;
+					mistakeCount = ((new File(mistakePath).list().length)/2)+"";
 				}
 				
 				String newFolderName = day+"（"+recordCount+"）（"+mistakeCount+"）";
