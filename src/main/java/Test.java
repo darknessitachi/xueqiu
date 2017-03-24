361a42104665bd9150a9a41851ff0f2a3d5f933c
yangruie63 <yangrui891210@gmail.com>
2017-03-24 10:46:37 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,33 +3,39 @@ import java.util.List;
 import util.Constants;
 import util.FileUtil;
 import util.StringUtil;
-
+import util.http.WebSpider;
 
 public class Test {
-	
+
 	public static void main(String args[]) {
-		//removeFolder();
 		
+		String str = WebSpider.getContentByClass("https://www.taoguba.com.cn/Article/1655031/1", "p_wenz");
+
+		for(String s:StringUtil.pattern(str, StringUtil.STOCK_PATTERN)){
+			System.out.println(s);
+		}
 	}
+	
+	
 
 	public static void removeFolder() {
-		
+
 		List<String> folderList = FileUtil.getFullFileNames(Constants.jgy_path);
-		for(String folderName : folderList){
-			if(folderName.indexOf("-") == 4){
-				
-				List<String> list = FileUtil.getFullFileNames(Constants.jgy_path+"/"+folderName);
-				
+		for (String folderName : folderList) {
+			if (folderName.indexOf("-") == 4) {
+
+				List<String> list = FileUtil
+						.getFullFileNames(Constants.jgy_path + "/" + folderName);
+
 				String file = FileUtil.fileLike(list, "down-");
-				
-				if(!StringUtil.isEmpty(file)){
-					FileUtil.delete(Constants.jgy_path+"/"+folderName+"/"+file);
+
+				if (!StringUtil.isEmpty(file)) {
+					FileUtil.delete(Constants.jgy_path + "/" + folderName + "/"
+							+ file);
 				}
 			}
 		}
-		
+
 	}
-	
-	
 
 }
